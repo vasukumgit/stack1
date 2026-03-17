a18796ff97fb8ed04de6de9eb2387a2dfc3bbcbf
vasukumgit <vasudevanleela25@gmail.com>
2026-03-17 17:55:10 +0530

    Update main.tf

--- terraform1/main.tf
+++ terraform1/main.tf
@@ -308,15 +308,3 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.app_alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.certificate_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.blue_tg.arn
-  }
-}
