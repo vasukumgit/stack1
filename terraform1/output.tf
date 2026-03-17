ebe9cb8c9356e90b4bd47ab0da30aee391629729
vasukumgit <vasudevanleela25@gmail.com>
2026-03-17 12:26:53 +0530

    Update output.tf

--- terraform1/output.tf
+++ terraform1/output.tf
@@ -1,3 +1,23 @@
-output "ec2_public_ip" {
-  value = aws_instance.tf_infras.public_ip
+output "blue_instance_public_ip" {
+  value = aws_instance.blue.public_ip
+}
+
+output "green_instance_public_ip" {
+  value = aws_instance.green.public_ip
+}
+
+output "blue_tg_arn" {
+  value = aws_lb_target_group.blue_tg.arn
+}
+
+output "green_tg_arn" {
+  value = aws_lb_target_group.green_tg.arn
+}
+
+output "listener_arn" {
+  value = aws_lb_listener.http.arn
+}
+
+output "alb_dns_name" {
+  value = aws_lb.app_alb.dns_name
 }
