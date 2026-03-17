8ec7dd8f20f2fb161529f454d25ce15865d6f8c3
vasukumgit <vasudevanleela25@gmail.com>
2026-03-17 14:44:02 +0530

    Update provider.tf

--- terraform1/provider.tf
+++ terraform1/provider.tf
@@ -1,13 +1,14 @@
 terraform {
-    required_version =">=1.5.0"
-    required _provider {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
+
+  required_version = ">= 1.0"
 }
 
-provider "aws"{
-    region = var.aws_region
+provider "aws" {
+  region = var.aws_region
 }
